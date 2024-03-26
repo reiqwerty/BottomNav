e5e0cf143009b30b02c5c720d1f6bce699bad76e
reiqwerty <reiqwerty69@gmail.com>
2024-03-26 15:38:31 +0700

    Initial Commit

--- app/src/main/java/com/example/bottomnav/BangunRuangAdapter.java
+++ app/src/main/java/com/example/bottomnav/BangunRuangAdapter.java
@@ -1,4 +1,74 @@
 package com.example.bottomnav;
 
-public class BangunRuangAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class BangunRuangAdapter extends RecyclerView.Adapter<BangunRuangAdapter.MyViewHolder> {
+    Context context;
+    ArrayList<BangunModel> modelBangunRuang = new ArrayList<>();
+    ItemClickListener mClickListener;
+
+    public BangunRuangAdapter(Context context, ArrayList<BangunModel> modelBangunRuang) {
+        this.context = context;
+        this.modelBangunRuang = modelBangunRuang;
+    }
+
+    @NonNull
+    @Override
+    public BangunRuangAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_bangun_ruang, parent, false);
+        return new BangunRuangAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BangunRuangAdapter.MyViewHolder holder, int position) {
+        holder.tvBangunRuang.setText(modelBangunRuang.get(position).getName());
+        Glide.with(context).load(modelBangunRuang.get(position).getImg()).into(holder.imgBangunRuang);
+    }
+
+    @Override
+    public int getItemCount() {
+        return modelBangunRuang.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ImageView imgBangunRuang;
+
+        TextView tvBangunRuang;
+
+        public MyViewHolder(View ItemView) {
+            super(ItemView);
+            imgBangunRuang = itemView.findViewById(R.id.imgBangunRuang);
+            tvBangunRuang= itemView.findViewById(R.id.tvBangunRuang);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mClickListener != null) mClickListener.onItemClick(v, getAdapterPosition());
+        }
+    }
+    BangunModel getItem(int id) {return modelBangunRuang.get(id);
+    }
+
+    void setClickListener(ItemClickListener itemClickListener){
+        this.mClickListener = itemClickListener;
+    }
+
+    public interface ItemClickListener{
+        void onItemClick(View view,int position);
+    }
 }
--- app/src/main/java/com/example/bottomnav/BangunRuangFragment.java
+++ app/src/main/java/com/example/bottomnav/BangunRuangFragment.java
@@ -1,64 +1,42 @@
 package com.example.bottomnav;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BangunRuangFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class BangunRuangFragment extends Fragment {
+import java.util.ArrayList;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class BangunRuangFragment extends Fragment implements BangunRuangAdapter.ItemClickListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public BangunRuangFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BangunRuangFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BangunRuangFragment newInstance(String param1, String param2) {
-        BangunRuangFragment fragment = new BangunRuangFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    ArrayList<BangunModel> modelBangunRuang = new ArrayList<>();
+    Context context;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_bangun_ruang,container,false);
+        RecyclerView rvBangunRuang = rootview.findViewById(R.id.rvBangunRuang);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+        modelBangunRuang.add(new BangunModel("Kubus","https://www.google.com/url?sa=i&url=http%3A%2F%2Fpersadapendidikan.blogspot.com%2F2010%2F11%2Fvolume-bangun-ruang-kubus.html&psig=AOvVaw1Lek1epVpGqJu54bw8HJ-D&ust=1711427149082000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCODE4ovJjoUDFQAAAAAdAAAAABAE"));
+        modelBangunRuang.add(new BangunModel("Bola","https://www.google.com/url?sa=i&url=https%3A%2F%2Froboguru.ruangguru.com%2Fquestion%2Fbangun-ruang-di-atas-disebut-_QU-FHWS0P3Y&psig=AOvVaw1HokreHCrc3VpcLRScdD_G&ust=1711427711072000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPDe1Z3LjoUDFQAAAAAdAAAAABAE"));
+        modelBangunRuang.add(new BangunModel("Balok","https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cnnindonesia.com%2Fedukasi%2F20230224175548-569-917575%2Fpengertian-sifat-dan-rumus-rumus-balok-lengkap-dengan-penjelasan&psig=AOvVaw1X1bb4m5pXebdyHKjottZu&ust=1711427779408000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNDcpLjLjoUDFQAAAAAdAAAAABAE"));
+        modelBangunRuang.add(new BangunModel("Limas","https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kompas.com%2Fskola%2Fread%2F2022%2F01%2F12%2F144952269%2Funsur-unsur-bangun-ruang-kerucut%3Fpage%3Dall&psig=AOvVaw1jrqMNI-I0Rfu4KZn2QJLQ&ust=1711519347729000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNCswsagkYUDFQAAAAAdAAAAABAE"));
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bangun_ruang, container, false);
+        rvBangunRuang.setLayoutManager(new LinearLayoutManager(getActivity()));
+        BangunRuangAdapter adapter = new BangunRuangAdapter(getContext(),modelBangunRuang);
+        rvBangunRuang.setAdapter(adapter);
+
+        return rootview;
+    }
+    @Override
+    public void onItemClick(View view, int position) {
+
     }
 }
